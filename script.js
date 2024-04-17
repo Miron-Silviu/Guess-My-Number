1fc2d4b5f6c717c0c8b57dbcfc57205938280504
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-17 23:08:27 +0300

    Build : Create listener event and define a logic in case of no number.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+// document.querySelector('.highscore').textContent = 30;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// Add event listener
 
-document.querySelector('.guess').value = 23;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
 
-document.querySelector('.highscore').textContent = 30;
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
