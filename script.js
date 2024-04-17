19c52fc1b6a4a5f8ba5497a67fc9a90e6009c33f
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-17 23:32:52 +0300

    Build : Implement logic in case of too high and in case of too low.

--- script.js
+++ script.js
@@ -18,5 +18,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = ' 📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
   }
 });
