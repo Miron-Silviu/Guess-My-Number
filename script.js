989b8d05ee4359b328b19e41ad6e2283fbd714fb
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-21 23:02:12 +0300

    Build : Implementing a logic when score is better then highscore.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
+let highscore = 0;
 // Add event listener to check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +27,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
+    // check if score is better then highscore
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // When guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -55,6 +62,7 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
